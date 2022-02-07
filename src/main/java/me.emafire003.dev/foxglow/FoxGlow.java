acd24c64ca86f39c7fbb6a424887a6b6647ba9dd
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2022-02-07 15:41:49 +0100

    Release Candidate 1

--- src/main/java/me.emafire003.dev/foxglow/FoxGlow.java
+++ src/main/java/me.emafire003.dev/foxglow/FoxGlow.java
@@ -1,21 +1,24 @@
-package net.fabricmc.example;
+package me.emafire003.dev.foxglow;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
 
-public class ExampleMod implements ModInitializer {
+public class FoxGlow implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public String MOD_ID = "foxglow";
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
+
+	public static final GameRules.Key<GameRules.IntRule> FOXGLOW_DURATION =
+			GameRuleRegistry.register("foxGlowDuration", GameRules.Category.MOBS, GameRuleFactory.createIntRule(10));
+
 }
---
+++ src/main/java/me.emafire003.dev/foxglow/mixin/FoxMixin.java
@@ -0,0 +1,33 @@
+package me.emafire003.dev.foxglow.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static me.emafire003.dev.foxglow.FoxGlow.FOXGLOW_DURATION;
+
+@Mixin(FoxEntity.class)
+public abstract class FoxMixin extends AnimalEntity {
+
+    protected FoxMixin(EntityType<? extends FoxEntity> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;finishUsing(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;)Lnet/minecraft/item/ItemStack;"))
+    protected void injectInTickMovementMethod(CallbackInfo ci) {
+        if(this.getEquippedStack(EquipmentSlot.MAINHAND).getItem().equals(Items.GLOW_BERRIES)){
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, this.world.getGameRules().getInt(FOXGLOW_DURATION)*20, 1, true, false));
+        }
+    }
+
+}
+
